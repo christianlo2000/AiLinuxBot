c6deb5148d80d33b794012212d9d783b50c08e16
Christian Lopez <christianlo_2000@yahoo.com>
2021-01-20 00:01:51 -0300

    agregado de corpus de comandos

---
+++ langDetect02.py
@@ -0,0 +1,16 @@
+import nltk
+import pycountry
+from nltk.stem import SnowballStemmer
+
+phrase_one = "good morning"
+phrase_two = "hey guy I'm bussy"
+
+tc = nltk.classify.textcat.TextCat() 
+guess_one = tc.guess_language(phrase_one)
+guess_two = tc.guess_language(phrase_two)
+
+guess_one_name = pycountry.languages.get(alpha_3=guess_one).name
+guess_two_name = pycountry.languages.get(alpha_3=guess_two).name
+print(guess_one_name)
+print(guess_two_name)
+
